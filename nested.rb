f8d9b6d925ee740b7bc14f3dcb1e523e6eedbf60
Danielle Jasper <dnj547@live.com>
2019-03-10 18:06:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]
+	programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash[yukihiro_matsumoto] = {
+	programmer_hash[:yukihiro_matsumoto] = {
 		:known_for => "Ruby",
 		:languages => ["LISP", "C"]
 	}
@@ -107,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
   programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+	programmer_hash
 end
 
 def adding_to_dennis
